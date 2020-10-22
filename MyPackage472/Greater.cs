5f44127893a42b99cc7752ec34487f71bd10f259
Anton Vakhrushev <anton.vakhrushev@fastdev.se>
2020-10-22 11:07:06 +0400

    My Package project

---
+++ MyPackage472/Greater.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPackage472
+{
+    public class Greater
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
